4a5f76427c805343d4ebcea19fcf59db16ec2a01
Liyema Mkebe <liyema.mkebe15@gmail.com>
2025-09-03 12:41:25 +0200

    Personal Finance Tracker App
    Interactivity & Comprehensive styling ,  next - working on alignment of (--close state--)

---
+++ src/static/js/newSidebar.js
@@ -0,0 +1,16 @@
+const body = document.querySelector("body"),
+        sidebar = body.querySelector(".sidebar"),
+        toggle = body.querySelector(".toggle"),
+        modeSwitch = body.querySelector(".toggle-switch"),
+        modeText = body.querySelector(".mode-text");
+
+
+        // Sidebar (close) event handler
+        toggle.addEventListener("click", () => {
+                sidebar.classList.toggle("close");
+        });
+        
+        // Dark-Mode event handler
+        modeSwitch.addEventListener("click", () => {
+                body.classList.toggle("dark");
+        });
