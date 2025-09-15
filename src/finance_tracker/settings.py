f83b5904b82b761b0a671c8f15c374bc10c76ff6
Liyema Mkebe <liyema.mkebe15@gmail.com>
2025-09-15 20:23:14 +0200

    Personal Finance Tracker App
    Styling changes:
    - Text changes in dark-mode
    - card-body dark-mode changes
    - custom bootstrap for <tables> for fluid ui

--- src/finance_tracker/settings.py
+++ src/finance_tracker/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'crispy_bootstrap5',
     'bootstrap_datepicker_plus',
     'django_filters',
+    'sass_processor',
     
     # Own
     'transactions.apps.TransactionsConfig',
@@ -140,6 +141,14 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
+
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'sass_processor.finders.CssFinder',
+]
+SASS_PROCESSOR_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
