ca53f1cd8cbc1073f4f165cf391baf0c0764d750
Liyema Mkebe <liyema.mkebe15@gmail.com>
2026-02-24 21:46:14 +0200

    PostgreSQL database intergrated.

---
+++ src/check.py
@@ -0,0 +1,3 @@
+import json 
+data=json.load(open('datadump.json')) 
+print(len(data), 'records')  
--- src/finance_tracker/settings.py
+++ src/finance_tracker/settings.py
@@ -13,12 +13,18 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 import mimetypes
+from decouple import config
+from dotenv import load_dotenv
+
 
 # MIME TYPE for SentiWise (.SVG-logo)
 mimetypes.add_type("image/svg+xml", ".svg", True) 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+ENV_PATH = Path(__file__).resolve().parent.parent
+load_dotenv(ENV_PATH / '.env')
 
 
 # Quick-start development settings - unsuitable for production
@@ -98,8 +104,12 @@ WSGI_APPLICATION = 'finance_tracker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
     }
 }
 
@@ -139,6 +149,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
